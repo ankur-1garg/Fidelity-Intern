35f6370f86d27ad317675b6e05c7501f189587d8
Shreyansh Srivastava <shreyanshsri19@gmail.com>
2025-02-13 19:44:30 +0530

    Updated Files and the project application.

--- Inventory_project/inventory/apps.py
+++ Inventory_project/inventory/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# Configuration class for the inventory app
 class InventoryConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "inventory"
+    default_auto_field = "django.db.models.BigAutoField"  # Default primary key field type
+    name = "inventory"  # Name of the app
